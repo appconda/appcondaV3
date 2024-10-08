3e1a1f25d9f8f2dda194e893177436bf8cd4e925
selimtan <stanoncloud@gmail.com>
2024-10-08 16:18:52 +0300

    .

---
+++ sdk-console/shell.js
@@ -0,0 +1,25 @@
+
+const manifest = require('./src/manifest');
+const appName = manifest.application.name;
+const libraryName = manifest.application.libraryName;
+const container_name = manifest.application.docker_container_name;
+
+var shell = require('shelljs');
+
+if (shell.exec('npm run wbuild').code !== 0) {
+  shell.echo('Build failed');
+  shell.exit(1);
+}
+
+// shell.cp('-Rf', './dist/index.js', '../runtime/app/realmocean/bios/lib/realmocean-sdk.js');
+
+// shell.exec(`docker cp  ./dist/index.js  realmocean-dev:/usr/src/code/app/realmocean/bios/lib/realmocean-sdk.js`);
+
+
+// shell.cd('/Users/selimtan/Organizations-New/celminov2');
+// shell.exec(`docker-compose restart`);
+// 
+// shell.cd('/Users/selimtan/Organizations-New/atlaas');
+// shell.exec(`docker-compose restart`);
+
+shell.echo(`All done.`);
